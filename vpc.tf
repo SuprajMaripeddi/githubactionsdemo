780c2bfdbf920c572f63aee601d823f8c73b77fd
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2023-08-25 08:31:54 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,8 @@
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.2.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demovpc"
+  }
 }
